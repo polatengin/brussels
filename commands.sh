8ef9ad0b44d22780a43886db43a1a0425ac872ab
Engin Polat <enpolat@microsoft.com>
2021-04-16 19:36:55 +0300

    command to create go project added

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
 # to run the development environment
 docker run --rm -it -v ${PWD}:/src --workdir "/src" golang:1.16.3-buster
+
+# to create the project
+go mod init github.com/${ACCOUNT_NAME}/${PROJECT_NAME}
