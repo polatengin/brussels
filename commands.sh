56e37a35a13996b265cf5f411d68331978fdd892
Engin Polat <enpolat@microsoft.com>
2021-04-16 19:37:12 +0300

    command to run the project added

--- commands.sh
+++ commands.sh
@@ -3,3 +3,6 @@ docker run --rm -it -v ${PWD}:/src --workdir "/src" golang:1.16.3-buster
 
 # to create the project
 go mod init github.com/${ACCOUNT_NAME}/${PROJECT_NAME}
+
+# to run the project
+go run .
